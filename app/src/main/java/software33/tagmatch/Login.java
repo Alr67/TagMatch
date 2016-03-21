a31afa21ecac4656b57fb296e9797143e496ef02
Rafa Lucena <rafael.lucena@est.fib.upc.edu>
2016-03-21 20:59:37 +0100

    Interfaz basica hecha + fuente de TagMatch!

---
+++ app/src/main/java/software33/tagmatch/Login.java
@@ -0,0 +1,29 @@
+package software33.tagmatch;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        TextView app_header = (TextView) findViewById(R.id.app_header);
+        /* MÈTODE PER FER SERVIR FONTS EXTERNES*/
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/LobsterTwo-BoldItalic.ttf");
+        app_header.setTypeface(face);
+
+    }
+
+}
