2098e92ef45dcf04a75b318c350dddfbe6b9124e
Rafa Lucena <rafael.lucena@est.fib.upc.edu>
2016-03-21 21:17:22 +0100

    ButterKnife

--- app/src/main/java/software33/tagmatch/Login.java
+++ app/src/main/java/software33/tagmatch/Login.java
@@ -7,23 +7,44 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Login extends AppCompatActivity {
+    /*Declaracions ButterKnife*/
+    @Bind(R.id.log) Button login;
+    @Bind(R.id.reg) Button reg;
+    @Bind(R.id.forg) Button forg;
+
+    @Bind(R.id.name) EditText username;
+    @Bind(R.id.pass) EditText pass;
+
+    @OnClick(R.id.log)
+    protected void validate_login() {
+        Toast.makeText(getApplicationContext(),username.getText().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        ButterKnife.bind(this); //Necessari per a q funcioni tot lo de ButterKnife
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         TextView app_header = (TextView) findViewById(R.id.app_header);
         /* MÈTODE PER FER SERVIR FONTS EXTERNES*/
         Typeface face= Typeface.createFromAsset(getAssets(), "fonts/LobsterTwo-BoldItalic.ttf");
         app_header.setTypeface(face);
-
+        /* MÈTODE PER FER SERVIR FONTS EXTERNES*/
     }
 
 }
