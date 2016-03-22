66983a10f55475814fa7630cae5ea4f51cebe72b
Alejandro Hidalgo <alejandro.hidalgo@est.fib.upc.edu>
2016-03-22 12:42:14 +0100

    interfaz hecha

---
+++ app/src/main/java/software33/tagmatch/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package software33.tagmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
