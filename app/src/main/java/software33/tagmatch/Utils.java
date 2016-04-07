0a0d632159d0c3d8c19d3e25dca8d84aaf90c031
Rafa Lucena <rafael.lucena@est.fib.upc.edu>
2016-04-07 19:44:51 +0200

    Funcio adquirir usuari logejat

---
+++ app/src/main/java/software33/tagmatch/Utils.java
@@ -0,0 +1,20 @@
+package software33.tagmatch;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Utils {
+    private static final String SH_PREF_NAME = "TagMatch_pref";
+
+    public ArrayList<String> getPersonalData(Context context){
+        ArrayList<String> data = new ArrayList<>();
+        SharedPreferences prefs = context.getSharedPreferences(SH_PREF_NAME, Context.MODE_PRIVATE);
+        data.add(prefs.getString("name", null));
+        data.add(prefs.getString("password", null));
+        return data;
+    }
+}
