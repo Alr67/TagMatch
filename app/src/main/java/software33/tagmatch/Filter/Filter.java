92812104ad8ffcf607865613cf3a082eac22d58b
Rafa <rafael.lucena@est.fib.upc.edu>
2016-04-21 21:00:56 +0200

    mas checkobx y el listener para todos + barra de separacion

--- app/src/main/java/software33/tagmatch/Filter/Filter.java
+++ app/src/main/java/software33/tagmatch/Filter/Filter.java
@@ -11,9 +11,9 @@ import android.widget.Toast;
 
 import software33.tagmatch.R;
 
-public class Filter extends AppCompatActivity {
+public class Filter extends AppCompatActivity implements View.OnClickListener {
 
-    private CheckBox filters;
+    private CheckBox ch1,ch2,ch3,ch4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +30,39 @@ public class Filter extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        addCheckBoxListener();
+        ch1 = (CheckBox) findViewById(R.id.checkBox);
+        ch1.setOnClickListener(this);
+        ch2 = (CheckBox) findViewById(R.id.checkBox2);
+        ch2.setOnClickListener(this);
+        ch3 = (CheckBox) findViewById(R.id.checkBox3);
+        ch3.setOnClickListener(this);
+        ch4 = (CheckBox) findViewById(R.id.checkBox4);
+        ch4.setOnClickListener(this);
     }
 
-    public void addCheckBoxListener() {
-
-        filters = (CheckBox) findViewById(R.id.checkBox);
-
-        filters.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (((CheckBox) v).isChecked()) {
-                    Toast.makeText(getApplicationContext(),"Bro, try Android :)", Toast.LENGTH_LONG).show();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case (R.id.checkBox):
+                if(((CheckBox) v).isChecked()){
+                    Toast.makeText(getApplicationContext(), "Bro, try Android :)", Toast.LENGTH_LONG).show();
                 }
-
-            }
-        });
-
+                break;
+            case (R.id.checkBox2):
+                if(((CheckBox) v).isChecked()){
+                    Toast.makeText(getApplicationContext(), "Bro, try Android :)", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case (R.id.checkBox3):
+                if(((CheckBox) v).isChecked()){
+                    Toast.makeText(getApplicationContext(), "Bro, try Android :)", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case (R.id.checkBox4):
+                if(((CheckBox) v).isChecked()){
+                    Toast.makeText(getApplicationContext(), "Bro, try Android :)", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
-
 }
