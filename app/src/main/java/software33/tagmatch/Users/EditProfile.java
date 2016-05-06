456caf5d5a4d32fec58bfb38789978074cb0effb
Alejandro Hidalgo <alejandro.hidalgo@est.fib.upc.edu>
2016-05-06 16:44:17 +0200

    Se borran filas :D

--- app/src/main/java/software33/tagmatch/Users/EditProfile.java
+++ app/src/main/java/software33/tagmatch/Users/EditProfile.java
@@ -6,9 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ import software33.tagmatch.R;
 public class EditProfile extends AppCompatActivity {
 
     TableLayout tl;
+    int lastTableID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +36,55 @@ public class EditProfile extends AppCompatActivity {
             }
         });
 
+        lastTableID = 0;
+
         tl = (TableLayout) findViewById(R.id.edit_profile_table);
 
         for (int i = 0; i <20; i++) {
-            TableRow row = new TableRow(this);
+            final TableRow row = new TableRow(this);
             TableRow.LayoutParams lp = new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT);
             row.setLayoutParams(lp);
             TextView tv = new TextView(this);
             tv.setText("qwe");
+            if(i == 0)
+                tv.setText("asdf");
             Button btn = new Button(this);
             btn.setBackground(getDrawable(android.R.drawable.ic_menu_delete));
-            row.addView(tv);
-            row.addView(btn);
-            tl.addView(row);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteRow(v);
+                }
+            });
+            String sBtn = String.valueOf(lastTableID);
+            btn.setText(sBtn);
+            btn.setTextColor(getResources().getColor(android.R.color.transparent));
+            row.addView(tv,0);
+            row.addView(btn,1);
+            tl.addView(row, lastTableID++);
         }
 
         //para que no se abra el teclado al entrar en la activity
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    private void refreshID() {
+        for(lastTableID = 0; lastTableID < tl.getChildCount(); lastTableID++){
+            TableRow row = (TableRow) tl.getChildAt(lastTableID);
+            Button btn = (Button) row.getChildAt(1);
+            btn.setText(String.valueOf(lastTableID));
+        }
+    }
+
+    public void addInterest(View view) {
+        String interest = ((EditText) findViewById(R.id.edit_profile_et_interests)).getText().toString();
+        
+    }
+
+    private void deleteRow(View v){
+        int rowID = Integer.parseInt(((Button) v).getText().toString());
+        tl.removeViewAt(rowID);
+        refreshID();
     }
 
     @Override
