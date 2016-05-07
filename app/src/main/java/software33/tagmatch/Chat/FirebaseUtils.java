cb477d6466098f956090047b2f3ea61de7170154
aleixpellisa <aleix.pellisa@est.fib.upc.edu>
2016-05-07 12:28:32 +0200

    Inici branca

--- app/src/main/java/software33/tagmatch/Chat/FirebaseUtils.java
+++ app/src/main/java/software33/tagmatch/Chat/FirebaseUtils.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 public abstract class FirebaseUtils {
 
-    private static Firebase myFirebaseRef = new Firebase("https://tagmatch.firebaseio.com/");
+    private static Firebase myFirebaseRef = new Firebase("https://torrid-torch-42.firebaseio.com/");
     private static Firebase chatsRef = myFirebaseRef.child("chats");
     private static Firebase usersRef = myFirebaseRef.child("users");
 
